5165501b250bde2e1449a00807b4b973979228f2
ABUBAKAR-AJU <168513413+ABUBAKAR-AJU@users.noreply.github.com>
2024-04-30 16:06:48 +0530

    fix: change database

--- back.js
+++ back.js
@@ -4,3 +4,9 @@ console.log("add on: ", name)
 const handlechange = (e) => {
     e.target.value
 }
+const name = ("this is a javascripts")
+console.log("add on: ", name)
+
+const handlechange = (e) => {
+    e.target.value
+}
